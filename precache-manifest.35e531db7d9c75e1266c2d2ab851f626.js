a3172b5f1fc752aba9c34df4a9a073a9ccf84513
Steve Choi <stevechoi93@gmail.com>
2019-01-22 13:58:06 -0800

    Updates

---
+++ precache-manifest.35e531db7d9c75e1266c2d2ab851f626.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a502fef40513bed0be41",
+    "url": "/todoreact/static/css/main.5f97801d.chunk.css"
+  },
+  {
+    "revision": "a502fef40513bed0be41",
+    "url": "/todoreact/static/js/main.a502fef4.chunk.js"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/todoreact/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "c72e28211de6a1967123",
+    "url": "/todoreact/static/js/runtime~main.c72e2821.js"
+  },
+  {
+    "revision": "f82eefeb5f86eac2001914285469b331",
+    "url": "/todoreact/index.html"
+  }
+];
